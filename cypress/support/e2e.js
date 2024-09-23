3305f534fb82ace911456ac947945c6706c56b95
Angus Chen <angus912584@gmail.com>
2024-09-23 15:42:24 +0800

    tests: use cypress v13

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,5 +1,5 @@
 // ***********************************************************
-// This example support/index.js is processed and
+// This example support/e2e.js is processed and
 // loaded automatically before your test files.
 //
 // This is a great place to put global configuration and
@@ -12,11 +12,6 @@
 // You can read more here:
 // https://on.cypress.io/configuration
 // ***********************************************************
+import installLogsCollector from 'cypress-terminal-report/src/installLogsCollector'
 
-// Import commands.js using ES2015 syntax:
-import './commands'
-
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
-
-import './e2e'
+installLogsCollector()
